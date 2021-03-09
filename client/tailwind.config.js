36d6b411007845a536e0cbae95cba7df9cdf28b4
Mandi Wise <hello@mandiwise.com>
2021-03-08 22:06:43 -0700

    Add basic Tailwind configuration.

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans:
+        "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+      mono: "source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace"
+    },
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
